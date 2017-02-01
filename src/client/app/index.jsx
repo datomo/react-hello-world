24ee48423691b8056929acdd86653089c2913922
datomo <dave.lengw@gmail.com>
2017-02-01 13:28:13 +0100

    finished setting up a clean react boilerplate with es05 and webpack

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+import React from 'react';
+import {render} from 'react-dom';
+
+import AwesomeComponent from './AwesomeComponent.jsx';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Hello World</p>
+				<AwesomeComponent />
+			</div>
+		);
+	}
+}
+
+const app = document.getElementById('app');
+render(<App/>, app);
